35fd3978fd5ebec08deec525dd00faa6559ccf0a
octavebahoun <octavebahoun@gmail.com>
2025-12-09 20:40:19 +0100

    railwal rules

--- config.php
+++ config.php
@@ -1,10 +1,21 @@
-<?php 
+<?php
 
-$host = "localhost";
-$db   = "scan_qr";
-$user = "root";      
-$pass = "";          
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
-
-
-?>
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
--- process.php
+++ process.php
@@ -1,24 +1,5 @@
 <?php
-// Afficher les erreurs PHP
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require_once 'config.php';
-
-// Connexion PDO sécurisée
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$db;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-    );
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
+require __DIR__ . '/config.php';
 
 // Vérifier si un token est envoyé
 if (!isset($_POST['token'])) {
@@ -33,22 +14,17 @@ $stmt->execute([$token]);
 $ticket = $stmt->fetch();
 
 // Gestion des cas
-
 if (!$ticket) {
-    // Le token n’existe pas du tout
     echo "Ticket inconnu 🚫";
     exit;
 }
 
-// Si le ticket existe :
+// Si le ticket existe
 if ($ticket['used'] == 1) {
-    // Déjà utilisé
     echo "Ticket déjà utilisé ❌";
 } else {
-    // Ticket valide → on le marque utilisé
     $update = $pdo->prepare("UPDATE tickets SET used = 1 WHERE token = ?");
     $update->execute([$token]);
 
-    echo "Ticket validé ";
+    echo "Ticket validé ✔️";
 }
-?>
